358ceb0f3d2c33cb79882a5872ec2ad90aab65e4
Coder DO as Impossible <95419810+SNV-008@users.noreply.github.com>
2022-03-16 09:30:16 +0530

    Update Swapping of digits.py

--- Swapping of digits.py
+++ Swapping of digits.py
@@ -1,12 +1,10 @@
-#first we will take x variable as input entered
-x = input("Enter a digit from 001-999, but while typing single digit then type two 0\'s before the number, and while typing double digit number type only one 0 behind them.")
-#Now i have taken 2 variables for listing as one will be changed,b ut one will not be changed as well when printed both should look like one another
-y = list(x)
-z = list(x)
-#changing the characters of both variable
-y[0] = x[2]
-y[1] = x[1]
-y[2] = x[0]
-#printing the both
-print("Original value =", z)
-print("Reversed value =", y)
+#Now we will take  x variable as input for first number
+x = int(input("Enter the value of First digit \n")
+#now we will take y variable as input for second number
+y = int(input("Enter the value of Second digit \n")
+#Now we will swap the digits
+x, y = y, x
+#now we will print both
+print("Reversed values are = ")
+print(x)
+print(y)
